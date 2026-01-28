f9f4f986af02174c73233548e30e86f072d2a7c0
Zick_Zibn <karpenko_ilia@list.ru>
2026-01-28 23:40:32 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(bean == bean2);
+
+        Cat cat1 = applicationContext.getBean("kiss_kiss", Cat.class);
+        Cat cat2 = applicationContext.getBean("kiss_kiss", Cat.class);
+        System.out.println(cat1 == cat2);
+    }
+}
